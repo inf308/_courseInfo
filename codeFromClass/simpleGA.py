8b3e488c9816cd88b7821e8625e104ac4a3b8c51
Cristyn Magnus <cmagnus@gmail.com>
2020-02-13 13:23:47 -0500

    Add to-dos for the rest of the algorithm

--- codeFromClass/simpleGA.py
+++ codeFromClass/simpleGA.py
@@ -37,3 +37,24 @@ def populationFitness(population):
 
 print(populationFitness(populationInit()))
 
+''' 5. Get probabilities '''
+''' 6. Get list with sum of probabilities so far 
+    (e.g. [1, 2, 1.5, 1] => [1, 3, 4.5, 5.5])'''
+''' 7. Make a function that randomly selects a member of the 
+    population based on probability'''
+''' 8. Make a function that makes a new chromosome whose first 
+    half comes from one random member of the population and 
+    second half comes from another random member of the 
+    population'''
+''' 9. Make a function that goes through each gene in a chromosome
+    and randomly decides whether or not to mutate. If it mutates,
+    replace 0 with 1 or vice versa. '''
+''' 10. For convenience, make a function that creates a child 
+    chromosome by combining 8 & 9'''
+''' 11. Make a function that makes a new population of children'''
+''' 12. Make a main function that initializes the population then
+    until a member of the population gets a perfect fitness, loops
+    through the process of replacing the old population with a 
+    new population of children. '''
+''' For convenience, make a function that finds and prints the
+    fittest member of the current population.'''
