034620b79c417208a8d8186b730a0c9f7e502689
Cristyn Magnus <cmagnus@gmail.com>
2020-02-25 12:50:59 -0500

    Make blob randomize if no args given

--- codeFromClass/blob/blob.py
+++ codeFromClass/blob/blob.py
@@ -3,19 +3,37 @@ import random
 
 class Blob:
     
-    def __init__(self, color, win):
+    def __init__(self, win, coords=None, size=None, speed=None, color=None):
         self.WIDTH = win.get_width()
         self.HEIGHT = win.get_height()
         self.order = ["x", "y", "size", "speed", "r", "g", "b"]
+        self.CROSSOVER = int(len(order)/2)
         self.chromosome = {}
-        self.chromosome["x"] = random.randrange(0, self.WIDTH)
-        self.chromosome["y"] = random.randrange(0, self.HEIGHT)
-        self.chromosome["size"] = random.randrange(4,20)
-        self.chromosome["speed"] = random.randrange(0,10)
-        self.chromosome["r"]
-        self.chromosome["g"]
-        self.chromosome["b"]
+
+        if coords is None: 
+            coords[0] = random.randrange(0, self.WIDTH)
+            coords[1] = random.randrange(0, self.HEIGHT)
+        self.chromosome["x"] = coords[0]
+        self.chromosome["y"] = coords[1]
+
+        if size is None:
+            size = random.randrange(4,20)
+        self.chromosome["size"] = size
+
+        if speed is None:
+            speed = random.randrange(0,10)
+        self.chromosome["speed"] = speed
+
+        if color is None:
+            color[0] = random.randint(0, 255)
+            color[1] = random.randint(0, 255)
+            color[2] = random.randint(0, 255)
+        self.chromosome["r"] = color[0]
+        self.chromosome["g"] = color[1]
+        self.chromosome["b"] = color[2]
+        
         self.win = win
+        self.rect = None
 
     def move(self):
         self.move_x = random.randrange(-1,2)
@@ -29,7 +47,19 @@ class Blob:
         if self.y < 0: self.y = 0
         elif self.y > self.HEIGHT: self.y = self.HEIGHT
     
-    
+
 
     def draw(self):
-        pygame.draw.circle(self.win, self.color, [self.x, self.y], self.size)
+        self.rect = pygame.draw.circle(self.win, self.color, [self.x, self.y], self.size)
+
+    def findMate(self, potentialMates):
+        mates = self.rect.collidelistall(potentialMates)
+        if mates < 0:
+            return False
+        
+        mate = random.choice(mates)
+        return potentialMates[mate]
+    
+    def splice(self, mate):
+        child = parent1[0:crossover] + parent2[crossover:]
+        return child
