5b283ffbd17144362a229baf5e77ab612c4d97f9
Cristyn Magnus <cmagnus@gmail.com>
2020-02-25 11:58:14 -0500

    Copy non-blob part of code

---
+++ codeFromClass/blob/main.py
@@ -0,0 +1,37 @@
+import pygame
+import random
+import blob
+
+WIDTH = 800
+HEIGHT = 600
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+game_display = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('Blob World')
+clock = pygame.time.Clock()
+
+def draw_environment(blobList):
+    game_display.fill(WHITE)
+    for blob in blobList:
+        blob.draw()
+    pygame.display.update()
+    for blob in blobList:
+        blob.move()
+
+    
+def main():
+    blobs = [Blob(RED, game_display), Blob(BLUE, game_display), Blob(GREEN, game_display)]
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        draw_environment(blobs)
+        clock.tick(60)
+
+if __name__ == '__main__':
+    main()
